9016d9bfe42905f65b6c43db2f76132f33b73d76
Gintare Mazeikyte <g.mazeikyte@gmail.com>
2021-02-02 12:20:04 +0200

    MainSpecialistPage ir SpecialistPageTest

---
+++ src/test/java/Tests/SpecialistPageTest.java
@@ -0,0 +1,17 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.internal.FindsByCssSelector;
+import org.openqa.selenium.internal.FindsByLinkText;
+
+import BaseTest.BaseTest;
+import Utils.WaitUtils;
+import page.MainAdminPage;
+import page.MainParentPage;
+
+public class SpecialistPageTest extends BaseTest{
+		
+}
