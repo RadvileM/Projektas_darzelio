8cac5030927a34913e8429c93bb33f4846ec6c65
RadvileM <radvile.maniliene@gmail.com>
2021-02-15 16:35:14 +0200

    new xtream version

--- src/main/java/page/MainAdminPage.java
+++ src/main/java/page/MainAdminPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
 
 import Utils.WaitUtils;
 
@@ -64,4 +65,5 @@ public class MainAdminPage extends AbstractPage{
 	}
 	
 
+			
 }
