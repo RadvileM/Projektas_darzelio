eb7603cc46c8c04675fa0117b68eedad9e6b0fc5
Amber <g.mazeikyte@gmail.com>
2021-03-01 19:41:39 +0200

    news

--- src/test/java/BaseTest/BaseTest.java
+++ src/test/java/BaseTest/BaseTest.java
@@ -5,25 +5,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 public abstract class BaseTest {
 	
 	protected static WebDriver driver;
 	
 	@BeforeClass
-	public void setUp(){
+	public void setUp() throws InterruptedException{
 		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://akademijait.vtmc.lt:8181/dis-app/");
+		  
 	}
 	
 
 	
-//	@Before
-//	public void openHomePage(){
-//		driver.get("http://akademijait.vtmc.lt:8181/dis-app/");
-//	}
+	//@BeforeMethod
+	//public void openHomePage(){
+	//driver.get("http://akademijait.vtmc.lt:8181/dis-app/");
+	//}
 	
 
 	@AfterClass
